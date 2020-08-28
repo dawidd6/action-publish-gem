452318d3181d477ec6be1aed9b6302a4cf1c76a6
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-28 20:57:13 +0200

    main: fix input and ensure file deletion

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby -W0
 # frozen_string_literal: true
 
 require 'fileutils'
@@ -7,7 +6,7 @@ def safe_system(*cmd)
   exit 1 unless system(*cmd)
 end
 
-api_key = ENV['INPUT_API_KEY'] || 'dummy'
+api_key = ENV['API_KEY'] || 'dummy'
 gemspec_files = Dir.glob('*.gemspec')
 credentials_dir_path = "#{Dir.home}/.gem"
 credentials_file_path = "#{credentials_dir_path}/credentials"
@@ -20,9 +19,13 @@ FileUtils.mkdir_p(credentials_dir_path)
 File.open(credentials_file_path, 'w') { |f| f.write(credentials) }
 FileUtils.chmod(0o600, credentials_file_path)
 
-gemspec_files.each do |gemspec_file|
-  gemspec = Gem::Specification.load(gemspec_file)
-  gem_file = gemspec.full_name + '.gem'
-  safe_system('gem', 'build', gemspec_file)
-  safe_system('gem', 'push', gem_file)
+begin
+  gemspec_files.each do |gemspec_file|
+    gemspec = Gem::Specification.load(gemspec_file)
+    gem_file = gemspec.full_name + '.gem'
+    safe_system('gem', 'build', gemspec_file)
+    safe_system('gem', 'push', gem_file)
+  end
+ensure
+  FileUtils.rm(credentials_file_path)
 end
